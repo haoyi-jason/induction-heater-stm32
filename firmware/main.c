8ef6e9a5d8f35c032d58de8b8abbaca5d126c2af
Jason <jjyeh1976@gmail.com>
2026-03-03 17:44:48 +0800

    Create firmware/main.c for STM32F103C8 induction heater controller

---
+++ firmware/main.c
@@ -0,0 +1,13 @@
+// STM32F103C8 Induction Heater Controller Code
+
+#include <stm32f10x.h>
+
+// Define your variables and functions here
+
+int main(void) {
+    // Initialization code
+
+    while (1) {
+        // Main control loop
+    }
+}
